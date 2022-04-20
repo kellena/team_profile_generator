d6bfec4ec6025b64a3541b629624efbe1efed640
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-19 22:01:14 -0500

    began writing vairables for index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require ('inquirer')
+const fs = require ('fs')
+const path = require ('path')
+
+const Engineer = require ('./lib/engineer')
+const Intern = require ('./lib/intern')
+const Manager = require ('./lib/manager')
