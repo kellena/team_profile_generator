53a555d65754a340bf58c4b8d562b975c412f1d9
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-10 22:17:06 -0500

    built out module exports for index.html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,4 +1,4 @@
-function generateTeamCards(teamArray){
+function generateCards(teamArray){
 
     function generateManager (employee){
 
@@ -79,4 +79,43 @@ function generateTeamCards(teamArray){
     );
     
     return employeeArray.join("");
+
 }
+
+
+module.exports = teamArray => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Venture Team Generator</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading">
+                    <h1 class="text-center">Venture Bros Inc.</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="team-area col-12 d-flex justify-content-center">
+                    ${generateCards(teamArray)}
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+    </html>`;
+};
